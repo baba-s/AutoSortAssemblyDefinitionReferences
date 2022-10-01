efcccbfa41f4a390c691d6b55b7f54c74de32bea
baba-s <shotababa1206@gmail.com>
2022-10-01 09:58:33 +0900

    fix

--- Editor/AutoSortAssemblyDefinitionReferences.cs
+++ Editor/AutoSortAssemblyDefinitionReferences.cs
@@ -1,8 +1,6 @@
 ﻿using System.IO;
 using System.Linq;
-using System.Text;
 using UnityEditor;
-using UnityEngine;
 
 namespace Kogane.Internal
 {
@@ -28,16 +26,17 @@ namespace Kogane.Internal
                     .ToArray()
                 ;
 
+            var currentDirectory = Directory
+                    .GetCurrentDirectory()
+                    .Replace( "\\", "/" )
+                ;
+
             foreach ( var assetPath in assemblyDefinitionPathArray )
             {
-                if ( excludeAssetPathArray.Contains( assetPath ) )
-                {
-                    continue;
-                }
+                if ( excludeAssetPathArray.Contains( assetPath ) ) continue;
 
-                var fullPath         = Path.GetFullPath( assetPath ).Replace( "\\", "/" );
-                var currentDirectory = Directory.GetCurrentDirectory();
-                var relativePath     = Path.GetRelativePath( currentDirectory, fullPath ).Replace( "\\", "/" );
+                var fullPath     = Path.GetFullPath( assetPath ).Replace( "\\", "/" );
+                var relativePath = Path.GetRelativePath( currentDirectory, fullPath ).Replace( "\\", "/" );
 
                 if ( !relativePath.StartsWith( "Assets/" ) &&
                      !relativePath.StartsWith( "Packages/" ) )
@@ -45,35 +44,7 @@ namespace Kogane.Internal
                     continue;
                 }
 
-                var json          = File.ReadAllText( assetPath );
-                var jsonData      = JsonUtility.FromJson<JsonAssemblyDefinition>( json );
-                var oldReferences = jsonData.references;
-
-                var newReferences = oldReferences
-                        .Select( x => x.Replace( "GUID:", "" ) )
-                        .Select( x => AssetDatabase.GUIDToAssetPath( x ) )
-                        .Where( x => !string.IsNullOrWhiteSpace( x ) )
-                        .OrderBy( x => Path.GetFileName( x ), new NaturalComparer() )
-                        .Select( x => AssetDatabase.AssetPathToGUID( x ) )
-                        .Where( x => !string.IsNullOrWhiteSpace( x ) )
-                        .Select( x => "GUID:" + x )
-                        .ToArray()
-                    ;
-
-                if ( newReferences.SequenceEqual( oldReferences ) )
-                {
-                    continue;
-                }
-
-                jsonData.references = newReferences;
-
-                var newJson = JsonUtility.ToJson( jsonData, true );
-
-                File.WriteAllText( assetPath, newJson, Encoding.UTF8 );
-
-                var asset = AssetDatabase.LoadAssetAtPath<Object>( assetPath );
-
-                EditorUtility.SetDirty( asset );
+                AssemblyDefinitionReferencesSorter.Sort( assetPath );
             }
 
             AssetDatabase.Refresh();

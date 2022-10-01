23cdced83fde994982e9f4bb82fa03c0692c1779
baba-s <shotababa1206@gmail.com>
2022-10-01 10:15:54 +0900

    fix

--- Editor/AutoSortAssemblyDefinitionReferences.cs
+++ Editor/AutoSortAssemblyDefinitionReferences.cs
@@ -6,6 +6,9 @@ namespace Kogane.Internal
 {
     internal sealed class AutoSortAssemblyDefinitionReferences : AssetPostprocessor
     {
+        private static string[] m_excludeAssetPathArray;
+        private static string   m_currentDirectory;
+
         private static void OnPostprocessAllAssets
         (
             string[] importedAssets,
@@ -14,40 +17,53 @@ namespace Kogane.Internal
             string[] movedFromAssetPaths
         )
         {
+            const string extension = ".asmdef";
+
+            if ( !importedAssets.Any( x => x.EndsWith( extension ) ) ) return;
+
             var assemblyDefinitionPathArray = importedAssets
-                    .Where( x => x.EndsWith( ".asmdef" ) )
+                    .Where( x => x.EndsWith( extension ) )
+                    .Where( x => !IsExclude( x ) )
+                    .Where( x => IsAssetsOrPackages( x ) )
                     .ToArray()
                 ;
+
+            m_excludeAssetPathArray = null;
+            m_currentDirectory      = null;
 
             if ( assemblyDefinitionPathArray.Length <= 0 ) return;
 
-            var excludeAssetPathArray = AutoSortAssemblyDefinitionReferencesSetting.instance.ExcludeList
-                    .Select( x => AssetDatabase.GetAssetPath( x ) )
-                    .ToArray()
-                ;
-
-            var currentDirectory = Directory
-                    .GetCurrentDirectory()
-                    .Replace( "\\", "/" )
-                ;
-
             foreach ( var assetPath in assemblyDefinitionPathArray )
             {
-                if ( excludeAssetPathArray.Contains( assetPath ) ) continue;
-
-                var fullPath     = Path.GetFullPath( assetPath ).Replace( "\\", "/" );
-                var relativePath = Path.GetRelativePath( currentDirectory, fullPath ).Replace( "\\", "/" );
-
-                if ( !relativePath.StartsWith( "Assets/" ) &&
-                     !relativePath.StartsWith( "Packages/" ) )
-                {
-                    continue;
-                }
-
                 AssemblyDefinitionReferencesSorter.Sort( assetPath );
             }
 
             AssetDatabase.Refresh();
+        }
+
+        private static bool IsExclude( string assetPath )
+        {
+            m_excludeAssetPathArray ??= AutoSortAssemblyDefinitionReferencesSetting.instance.ExcludeList
+                    .Select( x => AssetDatabase.GetAssetPath( x ) )
+                    .ToArray()
+                ;
+
+            return m_excludeAssetPathArray.Any( x => assetPath.StartsWith( x ) );
+        }
+
+        private static bool IsAssetsOrPackages( string assetPath )
+        {
+            m_currentDirectory ??= Directory
+                    .GetCurrentDirectory()
+                    .Replace( "\\", "/" )
+                ;
+
+            var fullPath     = Path.GetFullPath( assetPath ).Replace( "\\", "/" );
+            var relativePath = Path.GetRelativePath( m_currentDirectory, fullPath ).Replace( "\\", "/" );
+
+            return relativePath.StartsWith( "Assets/" ) ||
+                   relativePath.StartsWith( "Packages/" )
+                ;
         }
     }
 }
